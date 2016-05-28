9ec721e35b6b13b1186341d5f8a770e0fcaff291
Anuraag Yachamaneni <hello@anuraag.me>
2016-05-28 19:31:46 -0400

    Finish facebook integration with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {:sessions => "sessions", :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    match "login",  :to => "sessions#new",     :via => :get,    :as => :new_user_session
+    match "logout", :to => "sessions#destroy", :via => :delete, :as => :destroy_user_session
+  end
+
+  root "pages#index"
 end
