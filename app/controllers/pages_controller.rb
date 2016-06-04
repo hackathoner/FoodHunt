4da70cff3658d6dd506127c96dc303dc319792c4
Anuraag Yachamaneni <hello@anuraag.me>
2016-06-03 23:23:56 -0400

    Fix issue with timezones + UTC

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,19 @@
 class PagesController < ApplicationController
 
   def index
-    @date = Date.today
-    @recipes = Recipe.where("DATE(created_at) = ?", @date).ordered
+    @date = Time.now.to_date
+    @recipes = Recipe.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
 
   def paginate
     @date = Date.parse(params["date"])
-    @recipes = Recipe.where("DATE(created_at) = ?", @date).ordered
 
+    if @date == Date.today
+      @recipes = Recipe.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    else
+      @recipes = Recipe.where(created_at: @date.beginning_of_day..@date.end_of_day)
+    end
+    
     respond_to do |format|
       format.js { render layout: false, content_type: 'text/javascript' }
     end
