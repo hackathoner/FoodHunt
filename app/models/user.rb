68416ddfeb544c56c371c4a68a4c834a11b615aa
Anuraag Yachamaneni <hello@anuraag.me>
2016-06-07 22:08:57 -0400

    FUNC-1: Add profile image, name, and link to edit user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,25 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.name      = auth.info.name 
+      user.name      = auth.info.name
       user.email     = auth.info.email
       user.image     = auth.info.image
       user.password  = Devise.friendly_token[0,20]
     end
+  end
+
+  def submitted
+    Recipe.where(:user => self)
+  end
+
+  def voted
+    recipes = []
+
+    votes = Vote.where(:user => self)
+    votes.each do |vote|
+      recipes.push(vote.recipe)
+    end
+    recipes
   end
 
   def voted?(recipe)
